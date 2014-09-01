83d27abe0a105f831f850c57781c86ecbcfa453b
bdhurkett <bdhurkett@hotmail.com>
2014-09-01 23:03:51 +1000

    Essentially all the required to replace the old version, except exporting. Forgot to commit earlier.

--- Laser GPS Converter 2/Laser GPS Converter 2/Form1.Designer.cs
+++ Laser GPS Converter 2/Laser GPS Converter 2/Form1.Designer.cs
@@ -28,106 +28,152 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.list_Tracks = new System.Windows.Forms.ListBox();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
 			this.label1 = new System.Windows.Forms.Label();
-			this.button2 = new System.Windows.Forms.Button();
+			this.n_Offset = new System.Windows.Forms.NumericUpDown();
+			this.btn_Export = new System.Windows.Forms.Button();
+			this.txt_Details = new System.Windows.Forms.TextBox();
+			this.btn_Load = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
 			this.groupBox1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.n_Offset)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// listBox1
+			// list_Tracks
 			// 
-			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(12, 41);
-			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(153, 199);
-			this.listBox1.TabIndex = 0;
+			this.list_Tracks.FormattingEnabled = true;
+			this.list_Tracks.Location = new System.Drawing.Point(12, 41);
+			this.list_Tracks.Name = "list_Tracks";
+			this.list_Tracks.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.list_Tracks.Size = new System.Drawing.Size(185, 199);
+			this.list_Tracks.TabIndex = 0;
+			this.list_Tracks.SelectedIndexChanged += new System.EventHandler(this.list_Tracks_SelectedIndexChanged);
 			// 
 			// groupBox1
 			// 
 			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.numericUpDown1);
-			this.groupBox1.Controls.Add(this.button1);
-			this.groupBox1.Controls.Add(this.textBox1);
-			this.groupBox1.Location = new System.Drawing.Point(171, 13);
+			this.groupBox1.Controls.Add(this.n_Offset);
+			this.groupBox1.Controls.Add(this.btn_Export);
+			this.groupBox1.Controls.Add(this.txt_Details);
+			this.groupBox1.Location = new System.Drawing.Point(203, 13);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(362, 238);
+			this.groupBox1.Size = new System.Drawing.Size(330, 238);
 			this.groupBox1.TabIndex = 1;
 			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "groupBox1";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(7, 20);
-			this.textBox1.Multiline = true;
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(349, 183);
-			this.textBox1.TabIndex = 0;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(281, 209);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Export...";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// numericUpDown1
-			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(94, 212);
-			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(45, 20);
-			this.numericUpDown1.TabIndex = 2;
+			this.groupBox1.Text = "Track Details";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(6, 214);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(82, 13);
+			this.label1.Size = new System.Drawing.Size(127, 13);
 			this.label1.TabIndex = 3;
-			this.label1.Text = "Timezone offset";
+			this.label1.Text = "Timezone offset (vs UTC)";
 			// 
-			// button2
+			// n_Offset
 			// 
-			this.button2.Location = new System.Drawing.Point(12, 12);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 2;
-			this.button2.Text = "Load...";
-			this.button2.UseVisualStyleBackColor = true;
+			this.n_Offset.DecimalPlaces = 1;
+			this.n_Offset.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+			this.n_Offset.Location = new System.Drawing.Point(139, 212);
+			this.n_Offset.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+			this.n_Offset.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            -2147483648});
+			this.n_Offset.Name = "n_Offset";
+			this.n_Offset.Size = new System.Drawing.Size(45, 20);
+			this.n_Offset.TabIndex = 2;
+			this.n_Offset.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// btn_Export
+			// 
+			this.btn_Export.Location = new System.Drawing.Point(249, 209);
+			this.btn_Export.Name = "btn_Export";
+			this.btn_Export.Size = new System.Drawing.Size(75, 23);
+			this.btn_Export.TabIndex = 1;
+			this.btn_Export.Text = "Export...";
+			this.btn_Export.UseVisualStyleBackColor = true;
+			this.btn_Export.Click += new System.EventHandler(this.btn_Export_Click);
+			// 
+			// txt_Details
+			// 
+			this.txt_Details.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txt_Details.Location = new System.Drawing.Point(7, 20);
+			this.txt_Details.Multiline = true;
+			this.txt_Details.Name = "txt_Details";
+			this.txt_Details.ReadOnly = true;
+			this.txt_Details.Size = new System.Drawing.Size(317, 183);
+			this.txt_Details.TabIndex = 0;
+			// 
+			// btn_Load
+			// 
+			this.btn_Load.Location = new System.Drawing.Point(12, 12);
+			this.btn_Load.Name = "btn_Load";
+			this.btn_Load.Size = new System.Drawing.Size(75, 23);
+			this.btn_Load.TabIndex = 2;
+			this.btn_Load.Text = "Load...";
+			this.btn_Load.UseVisualStyleBackColor = true;
+			this.btn_Load.Click += new System.EventHandler(this.btn_Load_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.DefaultExt = "gpx";
+			this.openFileDialog1.FileName = "BridgeMin.dll";
+			this.openFileDialog1.Filter = "BridgeMin.dll|BridgeMin.dll|All files|*";
+			this.openFileDialog1.ReadOnlyChecked = true;
+			this.openFileDialog1.SupportMultiDottedExtensions = true;
+			this.openFileDialog1.Title = "Laser GPS Database";
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.DefaultExt = "gpx";
+			this.saveFileDialog1.SupportMultiDottedExtensions = true;
+			this.saveFileDialog1.Title = "Export GPX file...";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(545, 261);
-			this.Controls.Add(this.button2);
+			this.Controls.Add(this.btn_Load);
 			this.Controls.Add(this.groupBox1);
-			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.list_Tracks);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Laser GPS Converter V2";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Form_Load);
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.n_Offset)).EndInit();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox list_Tracks;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.NumericUpDown numericUpDown1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.NumericUpDown n_Offset;
+		private System.Windows.Forms.Button btn_Export;
+		private System.Windows.Forms.TextBox txt_Details;
+		private System.Windows.Forms.Button btn_Load;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
 	}
 }
 
--- Laser GPS Converter 2/Laser GPS Converter 2/Form1.cs
+++ Laser GPS Converter 2/Laser GPS Converter 2/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,142 @@ namespace Laser_GPS_Converter_2
 {
 	public partial class Form1 : Form
 	{
+		DataSet tracks;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form_Load(object sender, EventArgs e)
+		{
+			SetOffsetDefault();
+		}
+
+		private void SetOffsetDefault()
+		{
+			TimeZone zone = TimeZone.CurrentTimeZone;
+			n_Offset.Value = zone.GetUtcOffset(DateTime.Now).Hours;
+		}
+
+		private void btn_Load_Click(object sender, EventArgs e)
+		{
+			
+			SetUpOpenDialog();
+
+			list_Tracks.Items.Clear();
+
+			DialogResult result = openFileDialog1.ShowDialog();
+
+			if (result.Equals(DialogResult.Cancel))
+			{
+				return;
+			}
+
+			tracks = GetTracks();
+
+			if (tracks.Equals(null))
+			{
+				//exception occurred
+				return;
+			}
+
+			DataRowCollection dra = tracks.Tables["TrackPoint1"].Rows;
+			for (int i = dra.Count - 1; i > -1; i--)
+			{
+				string l = dra[i][2] + ": " + dra[i][3].ToString().TrimEnd(' ') + " (" + Math.Round(Convert.ToDouble(dra[i][13]) / 100000, 2) + " km)";
+				list_Tracks.Items.Add(l);
+			}
+		}
+
+		private DataSet GetTracks()
+		{
+			OleDbConnection conn = null;
+			DataSet t = new DataSet();
+			string strAccessConn = @"Provider=Microsoft.JET.OLEDB.4.0;Data Source=" + openFileDialog1.FileName + "; Jet OLEDB:Database Password=danger";
+			string strAccessSelect = "SELECT * FROM TrackPoint1";
+			try
+			{
+				conn = new OleDbConnection(strAccessConn);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: Failed to create a database connection. \n{0}", ex.Message);
+				return null;
+			}
+
+			try
+			{
+				OleDbCommand myAccessCommand = new OleDbCommand(strAccessSelect, conn);
+				OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(myAccessCommand);
+				conn.Open();
+				myDataAdapter.Fill(t, "TrackPoint1");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: Failed to create a database connection. \n{0}", ex.Message);
+				return null;
+			}
+			finally
+			{
+				conn.Close();
+			}
+			return t;
+		}
+
+		private void SetUpOpenDialog()
+		{
+			string defaultinstalldir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Laser GPS");
+			if (System.IO.Directory.Exists(defaultinstalldir))
+			{
+				openFileDialog1.InitialDirectory = defaultinstalldir;
+			}
+			else
+			{
+				openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+			}
+		}
+
+		private void btn_Export_Click(object sender, EventArgs e)
+		{
+			if (list_Tracks.SelectedIndices.Count == 0)
+			{
+				return;
+			}
+		}
+
+		private void list_Tracks_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			UpdateTrackDetails();
+		}
+
+		private void UpdateTrackDetails()
+		{
+			int i = list_Tracks.Items.Count - (list_Tracks.SelectedIndex + 1);
+
+			txt_Details.Clear();
+			DataRow dr = tracks.Tables[0].Rows[i];
+			txt_Details.AppendText("Started: " + dr[3].ToString().Trim());
+			txt_Details.AppendText(Environment.NewLine + "Ended:   " + dr[5].ToString().Trim());
+			txt_Details.AppendText(Environment.NewLine);
+
+			DateTime t1 = ParseDate(dr, 3);
+			DateTime t2 = ParseDate(dr, 5);
+			TimeSpan duration = t2 - t1;
+			txt_Details.AppendText(Environment.NewLine + "Duration: " + duration.ToString());
+
+			txt_Details.AppendText(Environment.NewLine + "Distance: " + (Convert.ToDouble(dr[13]) / 100).ToString("n0") + " m");
+		}
+
+		private DateTime ParseDate(DataRow dr, int i)
+		{
+			string s = dr[i].ToString();
+			s += n_Offset.Value > 0 ? '+' : '-';
+			s += ((int)n_Offset.Value).ToString("D2") + ':';
+			s += (n_Offset.Value - (int)n_Offset.Value == 0 ? "00" : "30");
+			DateTime t = new DateTime(0, DateTimeKind.Local);
+			t = DateTime.Parse(s);
+			return t;
 		}
 	}
 }
